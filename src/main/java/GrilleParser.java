36208ed72de3fe95dd321a2ac67ffc7b5d25169d
mateyendou <mateyendou@gmail.com>
2020-05-26 09:07:24 +0000

    Correction erreur PMD

--- src/main/java/GrilleParser.java
+++ src/main/java/GrilleParser.java
@@ -8,28 +8,35 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
 public class GrilleParser {
-    public static void parse(InputStream in, Grille grille) throws IOException {
-        Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
-        int dimension = grille.getDimension();
-        char[] buffer = new char[dimension];
-        for (int line = 0; line < dimension; line++) {
-            int lus = reader.read(buffer);
-            if (lus != dimension) {
-                throw new EOFException("format incorrect");
-            }
-            for (int i = 0; i < dimension; i++) {
-                grille.setValue(line, i, buffer[i]);
-            }
-            lus = reader.read(new char[1]);
-            if (lus != 1) {
-                throw new EOFException("pas de fin de ligne ? ligne=" + line);
-            }
-        }
-        reader.close();
+/**
+*Constructeur par defaut.
+*@param in le fichier à lire.
+*@param grille la grille sudoku à remplir.*/
+public static void parse(final InputStream in, final Grille grille) throws IOException {
+Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+int dimension = grille.getDimension();
+char[] buffer = new char[dimension];
+for (int line = 0; line < dimension; line++) {
+    int lus = reader.read(buffer);
+    if (lus != dimension) {
+        throw new EOFException("format incorrect");
+}
+for (int i = 0; i < dimension; i++) {
+    grille.setValue(line, i, buffer[i]);
+}
+lus = reader.read(new char[1]);
+if (lus != 1) {
+    throw new EOFException("pas de fin de ligne ? ligne=" + line);
     }
-
-    public static void parse(File f, Grille grille) throws IOException {
-        parse(new FileInputStream(f), grille);
-    }
+}
+reader.close();
+}
+/**
+*methode pour lire les données et remplir la grille.
+*@param f le fichier à lire.
+*@param grille la grille sudoku à remplir.*/
+public static void parse(final File f, final Grille grille) throws IOException {
+    parse(new FileInputStream(f), grille);
+}
 
 }
